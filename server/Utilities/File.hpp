f3b223bf176d105604b8f39572d3964fb04e0344
Josh Perkins <j11p205z@gmail.com>
2019-04-10 10:31:23 -0600

    [Server]: Added File class.

---
+++ server/Utilities/File.hpp
@@ -0,0 +1,28 @@
+/***********************************************************************************************
+ *                                                                                             *
+ *                 Project Name : CS3505 Final Project                                         *
+ *                                                                                             *
+ *                        File  : File.hpp                                                     *
+ *                                                                                             *
+ *                   Start Date : 04/10/19                                                     *
+ *                                                                                             *
+ *                      Modtime : 04/10/19                                                     *
+ *                                                                                             *
+ *---------------------------------------------------------------------------------------------*
+ * File:                                                                                       *
+ *   The file class is responsible for loading/saving local files.                             *
+ * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+#ifndef FILE_RS_H
+#define FILE_RS_H
+
+#include "Utilities/Log.hpp"
+
+namespace RS {
+	class File {
+	private:
+	public:
+	};
+}
+
+#endif
